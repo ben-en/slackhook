a76f503d88b40aa5599bb29f36cd5a0b4df8077c
Ben Ennis <nwbenennis@gmail.com>
2016-08-29 10:32:52 -0700

    new email

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='slackhook',
     version='0.1',
     author='Ben Ennis',
-    author_email='ben@outernet.is',
+    author_email='nwbenennis@gmail.com',
     description='python script to interact with slack webhooks',
     packages=[mod.__name__],
     entry_points={
